becdf6226687344259e4aeffe3422e440d2f78ed
Takato Kamikiya <kaosu763@gmail.com>
2026-04-24 14:04:22 +0900

    feat: タグ編集ページを実装 #17

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Tag;
 use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
 
 class TagController extends Controller
 {
@@ -20,5 +21,14 @@ class TagController extends Controller
     public function edit(Tag $tag)
     {
         return view('admin.tags.edit', compact('tag'));
+    }
+
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $tag->update([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/admin')->with('success', 'タグを更新しました');
     }
 }
