735f175195ef6081d01cf477aa52a03956fba6ba
Takato Kamikiya <kaosu763@gmail.com>
2026-04-27 20:16:58 +0900

    feat: API お問い合わせ登録機能の実装 #31

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'required' => ':attributeを入力してください。',
+    'string' => ':attributeは文字列で入力してください。',
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+    'email' => ':attributeの形式が正しくありません。',
+    'integer' => ':attributeは整数で入力してください。',
+    'array' => ':attributeは配列で入力してください。',
+
+    'attributes' => [
+        'first_name' => '姓',
+        'last_name' => '名',
+        'gender' => '性別',
+        'email' => 'メールアドレス',
+        'tel' => '電話番号',
+        'address' => '住所',
+        'building' => '建物名',
+        'category_id' => 'カテゴリー',
+        'detail' => 'お問い合わせ内容',
+        'tag_ids' => 'タグ',
+    ],
+];
