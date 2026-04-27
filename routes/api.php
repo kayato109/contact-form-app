20c2c97d9f77ac7c23a5dc54f66638941e26bd77
Takato Kamikiya <kaosu763@gmail.com>
2026-04-27 20:58:30 +0900

    feat: API お問い合わせ更新機能の実装 #33

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::prefix('v1')->group(function () {
-    Route::apiResource('contacts', ContactController::class)->only(['index', 'show', 'store']);
+    Route::apiResource('contacts', ContactController::class)->only(['index', 'show', 'store', 'update']);
 });
